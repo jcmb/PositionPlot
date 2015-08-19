b148bc7a67e575870e72a166a0569375170b7144
jcmb <Geoffrey@JCMBSoft.com>
2015-08-19 09:51:45 -0600

    removed TrimbleTools and using stdbuf

--- cgi-bin/start_single.sh
+++ cgi-bin/start_single.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#echo $1 $2 $3 $4 $5 $6 $7 $8
-./plot_single_cgi.sh $1 $2 $3 $4 $5 $6 $7 $8 $9&
+echo $1 $2 $3 $4 $5 $6 $7 $8
+stdbuf -o L ./plot_single_cgi.sh $1 $2 $3 $4 $5 $6 $7 $8 $9&
 disown
